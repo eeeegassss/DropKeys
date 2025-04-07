ff329ae96f31eb6a5508a1a76c2acffa1c699f61
eeeegassss <sagehanderson@icloud.com>
2025-04-07 16:40:26 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,19 @@
-const CACHE_NAME = 'dropkeys-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/css/style.css',
-  '/js/game.js',
-  '/manifest.json',
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('dropkeys-cache').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/game.js',
+        '/manifest.json'
+      ]);
+    })
   );
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => response || fetch(event.request))
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
   );
 });
