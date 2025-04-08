0e3d8b70754d990371293f88644054ac85bac0c6
eeeegassss <sagehanderson@icloud.com>
2025-04-08 01:40:59 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,13 @@
-self.addEventListener('install', e => {
+self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open('dropkeys-v1').then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/style.css',
-        '/game.js',
-        '/icon.png'
-      ]);
-    })
+    caches.open("dropkeys-cache").then(cache =>
+      cache.addAll(["index.html", "style.css", "game.js", "icon.png"])
+    )
   );
 });
 
-self.addEventListener('fetch', e => {
+self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
+    caches.match(e.request).then(response => response || fetch(e.request))
   );
 });
