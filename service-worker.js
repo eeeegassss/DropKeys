bc0d22d0ccb62ddc9353d5efdbc8fc19bb90e9eb
eeeegassss <sagehanderson@icloud.com>
2025-04-08 22:04:54 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,41 @@
+const CACHE_NAME = 'mini-game-hub-v1';
+const URLS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/game.js',
+  '/manifest.json',
+  '/icons/bolt.svg',
+  '/icons/star.svg',
+  '/images/darts.jpg',
+  '/images/snake.jpg',
+  '/images/colorcards.jpg',
+  '/images/colorconnect.jpg',
+  '/images/watersort.jpg',
+  '/images/blockfill.jpg'
+];
+
 self.addEventListener('install', event => {
   console.log('Service worker installed.');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(URLS_TO_CACHE))
+  );
 });
 
 self.addEventListener('fetch', event => {
-  // For offline support, cache logic would go here
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
 });
